40e5edb46b901f7bace6186b805dac1b9b9517d0
vagrant <vagrant@vagrant.vm>
2018-01-29 08:03:46 +0000

    first commit

---
+++ manage.py
@@ -0,0 +1,14 @@
+from flask_script import Manager, Server
+from catalog import app
+
+manager = Manager(app)
+
+manager.add_command('runserver', Server(
+                                        use_debugger=True,
+                                        use_reloader=True,
+                                        host='0.0.0.0',
+                                        port=9000))
+
+if __name__ == '__main__':
+
+    manager.run()
